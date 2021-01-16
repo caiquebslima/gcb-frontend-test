b02e6782dca36774ec972e5871803d9dc1dc4c37
caiquebslima <caiquebslima@gmail.com>
2021-01-16 15:50:22 -0300

    feat: added hamburguer menu feat: added main view area

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,10 @@
+import Menu from './Menu';
+
 export default function Header() {
   return (
     <header>
-      <div className='row'>
-        <h1 className='col-lg-5'>Healthy Food</h1>
-        <nav className='col-lg-7'>
-          <ul>
-            <a href='#'>
-              <li>HEALTHY RECIPES</li>
-            </a>
-            <a href='#'>
-              <li>BLOG</li>
-            </a>
-            <a href='#'>
-              <li>JOIN</li>
-            </a>
-            <a href='#'>
-              <li>
-                <button>REGISTER</button>
-              </li>
-            </a>
-          </ul>
-        </nav>
-      </div>
+      <h1>Healthy Food</h1>
+      <Menu />
     </header>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
+import Head from 'next/head';
 import Header from '../components/Header';
 import Illustration from '../components/icons/Illustration';
+import IconSearch from '../components/icons/IconSearch';
+import Menu from '../components/Menu';
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <div className='svg-container'>
-        <Illustration />
-      </div>
-    </main>
+    <div>
+      <Head>
+        <title>Healthy Food</title>
+        <meta charset='utf-8' />
+        <meta http-equiv='x-ua-compatible' content='ie=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width, height=device-height, viewport-fit=cover, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no'
+        />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+      </Head>
+
+      <main>
+        <Header />
+        <div className='svg-container'>
+          <Illustration />
+        </div>
+        <section className='search'>
+          <div className='search__title'>
+            <h2> Ready for Trying a new recipe?</h2>
+          </div>
+          <div className='search__input'>
+            <form>
+              <input type='text' placeholder='Search Healthy Recipes' />
+              <button type='submit'>
+                <IconSearch />
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
