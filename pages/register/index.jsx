72e83e6d99ecba552750c66ef145db29ed3c4b4e
caiquebslima <caiquebslima@gmail.com>
2021-01-16 07:48:31 -0300

    - feat: created file structure - feat: added sass and bootstrap - feat: changed svgs to jsx exports - improv: deleted unused files

---
+++ pages/register/index.jsx
@@ -0,0 +1,8 @@
+import BlocoFinal from '../../components/icons/BlocoFinal';
+import BlocoImage2 from '../../components/icons/BlocoImage2';
+
+function Register() {
+  return <div></div>;
+}
+
+export default Register;
